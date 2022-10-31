009f9d44ab14921f058fad64d875965d7a94914a
Erickype <67160282+Erickype@users.noreply.github.com>
2022-10-30 18:50:29 -0600

    Initial commit continuing create by scripting course

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float moveSpeed = 5;
+    public bool hasPowerup = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(moveSpeed * verticalInput * focalPoint.transform.forward);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateRandomPos(), enemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateRandomPos()
+    {
+        float spawnX = Random.Range(-spawnRange, spawnRange);
+        float spawny = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnX, 0, spawny);
+
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
