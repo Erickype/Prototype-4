669b7f180f75222c51d36dffb702fc104431b80d
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-01 12:39:58 -0500

    Update PowerUpEnum.cs
    powerup enum public variable

--- Assets/Scripts/PowerUpEnum.cs
+++ Assets/Scripts/PowerUpEnum.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PowerUpEnum : MonoBehaviour
 {
+    public enum PowerUp { Normal, HomingRocket }
+
+    public PowerUp powerUpSelector;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (powerUpSelector)
+        {
+            case PowerUp.Normal:
+                break;
+            case PowerUp.HomingRocket:
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
