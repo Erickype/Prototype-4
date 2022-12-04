64d32cb621a46693c27665a2478dca4853e6c178
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-04 00:23:36 -0500

    Update HominMisile.cs
    Orientate misile to target

--- Assets/Scripts/PowerUps/HominMisile.cs
+++ Assets/Scripts/PowerUps/HominMisile.cs
@@ -12,6 +12,16 @@ namespace Assets.Scripts.PowerUps
         private float misileStrength = 15.0f;
         private float aliveTimer = 5.0f;
 
+        private void Update()
+        {
+            if(homing && target != null)
+            {
+                Vector3 movingDirection = (target.transform.position - transform.position).normalized;
+                transform.position += speed * Time.deltaTime * movingDirection;
+                transform.LookAt(transform.position);
+            }
+        }
+
         public void Fire(Transform newTarget)
         {
             target = newTarget;
