68fd2788298c20af2b1c05e5c5b5e2f0bcbb211a
Erickype <67160282+Erickype@users.noreply.github.com>
2022-12-04 00:16:10 -0500

    Update HominMisile.cs
    Variables declaration and clean up

--- Assets/Scripts/PowerUps/HominMisile.cs
+++ Assets/Scripts/PowerUps/HominMisile.cs
@@ -5,32 +5,12 @@ namespace Assets.Scripts.PowerUps
 {
     public class HominMisile : MonoBehaviour
     {
-        private Rigidbody enemyRb;
+        private Transform target;
+        private float speed = 100;
+        private bool homing;
 
-        private Transform playerTrans;
-        public float speed = 100;
+        private float misileStrength = 15.0f;
+        private float aliveTimer = 5.0f;
 
-        // Use this for initialization
-        void Start()
-        {
-             playerTrans = GameObject.Find("Player").transform;
-             enemyRb = GetComponent<Rigidbody>(); 
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            Vector3 lookDirection = (transform.position - playerTrans.position).normalized;
-
-            enemyRb.AddForce(speed * lookDirection);
-
-            StartCoroutine(DestroyCountdown());
-        }
-
-        IEnumerator DestroyCountdown()
-        {
-            yield return new WaitForSeconds(7);
-            Destroy(gameObject);
-        }
     }
 }
